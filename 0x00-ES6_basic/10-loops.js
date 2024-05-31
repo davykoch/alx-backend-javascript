40ed6f5b2c47130a5a0ea931686d7767c4874414
davykoch <koechdavis24@gmail.com>
2024-05-31 22:21:41 +0300

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const arr = [];
-	for (const val in array) {
-	  arr.push(appendString + val);
-	}
-  
-	return arr;
+  const arr = [];
+  for (const val of array) {
+    arr.push(appendString + val);
   }
+
+  return arr;
+}
