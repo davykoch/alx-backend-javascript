c6714e1dae737403006fa09761165906ec7a8f15
davykoch <koechdavis24@gmail.com>
2024-06-04 12:21:39 +0300

    EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+export default class EvCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+    return new Species();
+  }
+}
